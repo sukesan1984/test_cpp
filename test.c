a750867883ffb63358fa25dc2b269e6c206dcdbb
Kosuke Takami <takami.kosuke@dena.com>
2013-09-08 12:51:40 +0900

    remove strlen

--- test.c
+++ test.c
@@ -27,26 +27,15 @@ char* makeText(){
 };
 
 char* mystrstr(char* text, char* pat){
-    int textLength = strlen(text);
-    int patLength = strlen(pat);
-    if(textLength < patLength) {
-        return NULL;
-    }
-    if(patLength == 0){
-        return text;
-    }
-
-    char first = pat[0];
-
-    int i = 0;
-
-    for(i=0; i < textLength - 1; i++){
-        //一文字目が見つかるまで
-        if (first != text[i]){
-            continue;
-        }
-        if(isMatch(&text[i], pat)){
-            return &text[i];
+    //text:abcdefghijklmn
+    //pat:defgh
+    //a b c d
+    //      d
+    while(*text != '\0'){
+        if(!isMatch(text, pat)){
+            text++;
+        } else {
+            return text;
         }
     }
     return NULL;
@@ -61,51 +50,51 @@ int main(void)
         char    s5[] = "";                      /* \0 */
         char    *sp;
 
-        printf("true:%d\n", isMatch("abcdef", "ab"));
-        printf("false:%d\n", isMatch("abcdef", "ce"));
-        printf("false:%d\n", isMatch("abcdef", "abcdefg"));
+        /*printf("true:%d\n", isMatch("abcdef", "ab"));*/
+        /*printf("false:%d\n", isMatch("abcdef", "ce"));*/
+        /*printf("false:%d\n", isMatch("abcdef", "abcdefg"));*/
 
-        sp = mystrstr(s1,s2);
-        printf("(defghijkmln)一致する場合 : %s\n",sp);
-        sp = mystrstr(s1,s3);
-        printf("(null)一致しない場合 : %s\n",sp);
-        sp = mystrstr(s1,s4);
-        printf("(null)検索対象文字列より長い場合 : %s\n",sp);
-        sp = mystrstr(s1,s5);
-        printf("\\0 : %s\n",sp);
+        /*sp = mystrstr(s1,s2);*/
+        /*printf("(defghijkmln)一致する場合 : %s\n",sp);*/
+        /*sp = mystrstr(s1,s3);*/
+        /*printf("(null)一致しない場合 : %s\n",sp);*/
+        /*sp = mystrstr(s1,s4);*/
+        /*printf("(null)検索対象文字列より長い場合 : %s\n",sp);*/
+        /*sp = mystrstr(s1,s5);*/
+        /*printf("\\0 : %s\n",sp);*/
 
         
-        /*char *text = makeText();*/
-        /*char *pat = "sukesan";*/
+        char *text = makeText();
+        char *pat = "sukesan";
 
-        /*int start;*/
-        /*int end;*/
-        /*struct timeval start_timeval, end_timeval;*/
-        /*double sec_timeofday;*/
+        int start;
+        int end;
+        struct timeval start_timeval, end_timeval;
+        double sec_timeofday;
 
-        /*printf("本物\n");*/
+        printf("本物\n");
 
-        /*gettimeofday( &start_timeval, NULL );*/
+        gettimeofday( &start_timeval, NULL );
 
-        /*sp = strstr(text, pat);*/
-        /*printf("%s\n", sp);*/
+        sp = strstr(text, pat);
+        printf("%s\n", sp);
 
-        /*gettimeofday( &end_timeval, NULL );*/
-        /*sec_timeofday = (end_timeval.tv_sec - start_timeval.tv_sec)*/
-                /*+ (end_timeval.tv_usec - start_timeval.tv_usec) / 1000000.0;*/
-        /*printf("%f\n", sec_timeofday);*/
+        gettimeofday( &end_timeval, NULL );
+        sec_timeofday = (end_timeval.tv_sec - start_timeval.tv_sec)
+                + (end_timeval.tv_usec - start_timeval.tv_usec) / 1000000.0;
+        printf("%f\n", sec_timeofday);
 
 
-        /*printf("俺\n");*/
-        /*gettimeofday( &start_timeval, NULL );*/
+        printf("俺\n");
+        gettimeofday( &start_timeval, NULL );
 
-        /*sp = mystrstr(text, pat);*/
-        /*printf("%s\n", sp);*/
+        sp = mystrstr(text, pat);
+        printf("%s\n", sp);
 
-        /*gettimeofday( &end_timeval, NULL );*/
-        /*sec_timeofday = (end_timeval.tv_sec - start_timeval.tv_sec)*/
-                /*+ (end_timeval.tv_usec - start_timeval.tv_usec) / 1000000.0;*/
-        /*printf("%f\n", sec_timeofday);*/
+        gettimeofday( &end_timeval, NULL );
+        sec_timeofday = (end_timeval.tv_sec - start_timeval.tv_sec)
+                + (end_timeval.tv_usec - start_timeval.tv_usec) / 1000000.0;
+        printf("%f\n", sec_timeofday);
         
         return 0;
 }
